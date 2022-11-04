6e5e0f225d2b236fa5044f726cab05fb2db3e026
jpkha <jeanphilippe.kha@gmail.com>
2022-11-04 15:14:14 +0000

    Deploying to gh-pages from @ jpkha/nextjs12-storyblok@d3eb315157d3d704d2e5f80479531d3d329f7a0b 🚀

---
+++ _next/static/FX0MDKHqM22suOacleCLT/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-8a27e5c4f73f7952.js"],"/_error":["static/chunks/pages/_error-e4f561a102d9bb14.js"],"/[...slug]":["static/chunks/pages/[...slug]-30409b7a862e006a.js"],sortedPages:["/","/_app","/_error","/[...slug]"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
